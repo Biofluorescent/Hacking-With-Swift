f5faab22bcbdef8c9d6bfdfee447d6e753ecec4f
tquesenberry <tanner.quesenberry@gmail.com>
2019-02-18 12:20:42 -0800

    Storyboard setup

---
+++ Project38/Project38/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project38
+//
+//  Created by Tanner Quesenberry on 2/18/19.
+//  Copyright © 2019 Tanner Quesenberry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
