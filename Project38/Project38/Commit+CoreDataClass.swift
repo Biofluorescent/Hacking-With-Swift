953cf6076280e7bf7ba55f7be485f72a62c46ff4
tquesenberry <tanner.quesenberry@gmail.com>
2019-02-18 18:09:58 -0800

    Fetch api data and create Commit objects

---
+++ Project38/Project38/Commit+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Commit+CoreDataClass.swift
+//  Project38
+//
+//  Created by Tanner Quesenberry on 2/18/19.
+//  Copyright © 2019 Tanner Quesenberry. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Commit)
+public class Commit: NSManagedObject {
+
+}
--- Project38/Project38/ViewController.swift
+++ Project38/Project38/ViewController.swift
@@ -24,8 +24,12 @@ class ViewController: UITableViewController {
                 print("Unresolved error \(error)")
             }
         }
+        
+        //Perform network request in background
+        performSelector(inBackground: #selector(fetchCommits), with: nil)
     }
 
+    
     func saveContext() {
         if container.viewContext.hasChanges {
             do {
@@ -36,5 +40,42 @@ class ViewController: UITableViewController {
         }
     }
 
+    
+    //Downloads the URL into a String object then converts to array of object using SwiftyJSON
+    @objc func fetchCommits() {
+        if let data = try? String(contentsOf: URL(string: "https://api.github.com/repos/apple/swift/commits?per_page=100")!) {
+            //give data to SwiftyJSON to parse
+            let jsonCommits = JSON(parseJSON: data)
+            
+            //read the commits back out
+            let jsonCommitArray = jsonCommits.arrayValue
+            
+            print("Received \(jsonCommitArray.count) new commits.")
+            
+            DispatchQueue.main.async { [unowned self] in
+                for jsonCommit in jsonCommitArray {
+                    //Create Commit object inside managed object context
+                    let commit = Commit(context: self.container.viewContext)
+                    //Configure object using JSON data
+                    self.configure(commit: commit, usingJSON: jsonCommit)
+                }
+                
+                self.saveContext()
+            }
+        }
+    }
+    
+    
+    //Configures Commit object from json using SwiftyJSON
+    func configure(commit: Commit, usingJSON json: JSON) {
+        commit.sha = json["sha"].stringValue
+        commit.message = json["commit"]["message"].stringValue
+        commit.url = json["html_url"].stringValue
+        
+        let formatter = ISO8601DateFormatter()
+        commit.date = formatter.date(from: json["commit"]["committer"]["date"].stringValue) ?? Date()
+    }
+    
+    
 }
 
