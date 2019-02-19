8a3f334f0f64a55e7df2a2a09aef1d37d2d25ad3
tquesenberry <tanner.quesenberry@gmail.com>
2019-02-18 16:26:07 -0800

    Load into managed context for read and write

--- Project38/Project38/ViewController.swift
+++ Project38/Project38/ViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UITableViewController {
 
+    var container: NSPersistentContainer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        //Create container
+        container = NSPersistentContainer(name: "Project38")
+        //Load saved database if exists or create one
+        container.loadPersistentStores { storeDescription, error in
+            if let error = error {
+                print("Unresolved error \(error)")
+            }
+        }
     }
 
+    func saveContext() {
+        if container.viewContext.hasChanges {
+            do {
+                try container.viewContext.save()
+            } catch {
+                print("An error occured while saving: \(error)")
+            }
+        }
+    }
 
 }
 
