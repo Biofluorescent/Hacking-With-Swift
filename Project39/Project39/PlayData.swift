895d38869e5354a5b3d49549eb61f84fb0550956
tquesenberry <tanner.quesenberry@gmail.com>
2019-02-20 17:33:42 -0800

    User filter by word frequency or substring

--- Project39/Project39/PlayData.swift
+++ Project39/Project39/PlayData.swift
@@ -12,6 +12,7 @@ class PlayData {
     var allWords = [String]()
     //Items can only be added once but keeps track of # of times trying to add or remove item
     var wordCounts: NSCountedSet!
+    private(set) var filteredWords = [String]()
     
     init() {
         if let path = Bundle.main.path(forResource: "plays", ofType: "txt") {
@@ -27,5 +28,24 @@ class PlayData {
                 allWords = sorted as! [String]
             }
         }
+        
+        filteredWords = allWords
     }
+    
+    
+    func applyUserFilter(_ input: String) {
+        if let userNumber = Int(input) {
+            //Creates array out of words with a count greater or equal to user number
+            applyFilter { self.wordCounts.count(for: $0) >= userNumber}
+        } else {
+            //Creates array out of words that contain the user's text as a substring
+            applyFilter { $0.range(of: input, options: .caseInsensitive) != nil}
+        }
+    }
+    
+    //Accepts a function that accepts a string and returns a boolean
+    func applyFilter(_ filter: (String) -> Bool) {
+        filteredWords = allWords.filter(filter)
+    }
+    
 }
