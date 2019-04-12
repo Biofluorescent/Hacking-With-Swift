f53b9415f664cadd7df9140803acab495a93e9cc
tquesenberry <tanner.quesenberry@gmail.com>
2019-04-11 19:36:07 -0700

    Project 33: Programmatic Auto layout of record screen, Record Audio, Save Audio to Directory File

---
+++ Project33/Project33/RecordWhistleViewController.swift
@@ -0,0 +1,172 @@
+//
+//  RecordWhistleViewController.swift
+//  Project33
+//
+//  Created by Tanner Quesenberry on 4/11/19.
+//  Copyright © 2019 Tanner Quesenberry. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class RecordWhistleViewController: UIViewController, AVAudioRecorderDelegate {
+
+    var stackView: UIStackView!
+    
+    var recordButton: UIButton!
+    var recordingSession: AVAudioSession!
+    var whistleRecorder: AVAudioRecorder!
+    
+    //MARK: - UI Loading Functions
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Record your whistle"
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "Record", style: .plain, target: nil, action: nil)
+        
+        recordingSession = AVAudioSession.sharedInstance()
+        
+        do {
+            try recordingSession.setCategory(.playAndRecord, mode: .default)
+            try recordingSession.setActive(true)
+            recordingSession.requestRecordPermission { [unowned self] allowed in
+                DispatchQueue.main.async {
+                    if allowed {
+                        self.loadRecordingUI()
+                    }else {
+                        self.loadFailUI()
+                    }
+                }
+            }
+        }catch {
+            self.loadFailUI()
+        }
+
+    }
+    
+    
+    func loadRecordingUI(){
+        recordButton = UIButton()
+        recordButton.translatesAutoresizingMaskIntoConstraints = false
+        recordButton.setTitle("Tap to record", for: .normal)
+        recordButton.titleLabel?.font = UIFont.preferredFont(forTextStyle: .title1)
+        recordButton.addTarget(self, action: #selector(recordTapped), for: .touchUpInside)
+        stackView.addArrangedSubview(recordButton)
+    }
+    
+    
+    func loadFailUI(){
+        let failLabel = UILabel()
+        failLabel.font = UIFont.preferredFont(forTextStyle: .headline)
+        failLabel.text = "Recording failed: please ensure the app has access to your microphone."
+        //Wrap over as many lines as needed
+        failLabel.numberOfLines = 0
+        
+        //Use this method, never add a subview to a stack view directly
+        stackView.addArrangedSubview(failLabel)
+    }
+    
+    //Do not call this function directly. View controller calls it when view is nil and requested
+    override func loadView() {
+        view = UIView()
+        
+        view.backgroundColor = UIColor.gray
+        
+        stackView = UIStackView()
+        stackView.spacing = 30
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = UIStackView.Distribution.fillEqually
+        stackView.alignment = .center
+        stackView.axis = .vertical
+        view.addSubview(stackView)
+        
+        stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
+    //MARK: - Recording Relation Functions
+    
+    func startRecording() {
+        view.backgroundColor = UIColor(red: 0.6, green: 0, blue: 0, alpha: 1)
+        recordButton.setTitle("Tap to stop", for: .normal)
+        
+        //Where sound will be saved
+        let audioURL = RecordWhistleViewController.getWhistleURL()
+        print(audioURL.absoluteString)
+        
+        //Setting dictionary for recorder
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
+        
+        do {
+            //Create Recorder object, set delegate, call record
+            whistleRecorder = try AVAudioRecorder(url: audioURL, settings: settings)
+            whistleRecorder.delegate = self
+            whistleRecorder.record()
+        } catch {
+            finishRecording(success: false)
+        }
+    }
+    
+    
+    func finishRecording(success: Bool) {
+        view.backgroundColor = UIColor(red: 0, green: 0.6, blue: 0, alpha: 1)
+        
+        //Stop and delete recorder object
+        whistleRecorder.stop()
+        whistleRecorder = nil
+        
+        if success {
+            recordButton.setTitle("Tap to Re-record", for: .normal)
+            //Show new button to progress to next submission stage
+            navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .plain, target: self, action: #selector(nextTapped))
+        }else {
+            recordButton.setTitle("Tap to Record", for: .normal)
+            
+            //Show error message to user
+            let ac = UIAlertController(title: "Record Failed", message: "There was a problem recording your whistle; please try again", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+    }
+    
+    @objc func nextTapped() {
+        
+    }
+    
+    //When record button pressed
+    @objc func recordTapped() {
+        if whistleRecorder == nil {
+            startRecording()
+        }else {
+            finishRecording(success: true)
+        }
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if !flag {
+            finishRecording(success: false)
+        }
+    }
+    
+    //MARK: - Directory functions
+    
+    //Class keyword means you can call them on the class not on instances of the class
+    //Similar to static keyword of structs
+    class func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
+    }
+    
+    class func getWhistleURL() -> URL {
+        return getDocumentsDirectory().appendingPathComponent("whistle.m4a")
+    }
+    
+}
---
+++ Project33/Project33/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Project33
+//
+//  Created by Tanner Quesenberry on 4/11/19.
+//  Copyright © 2019 Tanner Quesenberry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "What's that Whistle?"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addWhistle))
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "Home", style: .plain, target: nil, action: nil)
+    }
+
+
+    @objc func addWhistle() {
+        let vc = RecordWhistleViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
